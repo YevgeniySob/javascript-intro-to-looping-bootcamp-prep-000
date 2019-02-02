46befbc870037860941c5a25ba707ff418611f99
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-02 21:49:41 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,8 +2,9 @@ function forLoop(arr){
   for(var i = 0; i < 25; i++){
     if (i === 1){
       arr.push('I am 1 strange loop.')
+    } else {
+        arr[i] = ("I am ${i} strange loops.")
     }
-    arr[i] = ("I am ${i} strange loops.")
   }
   return arr
 }
