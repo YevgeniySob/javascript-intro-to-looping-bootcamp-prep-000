39ab0d0dbfd83d0b7614cedcbe015eb2f44f3a8e
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-02 21:43:08 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,6 +16,11 @@ function whileLoop(n){
   return 'done'
 }
 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
 function doWhileLoop(n){
   do {
     console.log("I run once regardless.")
